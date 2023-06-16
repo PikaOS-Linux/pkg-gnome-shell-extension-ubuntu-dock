bfe06b24b6c39d75815c16d3a70fd276f904a406
ferrreo <harderthanfire@gmail.com>
2023-06-16 19:07:33 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,5 @@
 DEBIAN_FRONTEND=noninteractive
 
-# Add dependent repositories
-wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
-add-apt-repository https://ppa.pika-os.com
-add-apt-repository ppa:pikaos/pika
-add-apt-repository ppa:kubuntu-ppa/backports
 # Clone Upstream
 tar -xf ./gnome-shell-extension-ubuntu-dock_79.orig.tar.xz -C ./
 rm -rfv ./gnome-shell-extension-ubuntu-dock-79ubuntu2/debian
@@ -12,8 +7,6 @@ cp -rvf ./debian ./gnome-shell-extension-ubuntu-dock-79ubuntu2/
 cd ./gnome-shell-extension-ubuntu-dock-79ubuntu2
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
